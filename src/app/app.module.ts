ba444fa42f74ecedb221188800e7e9a688e1559f
Mathias-Boulay <mathiasboulay@free.fr>
2023-03-01 12:23:09 +0100

    Chore: Scaffhold application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FestivalsListComponent } from './components/festival/festivals-list/festivals-list.component';
+import { FestivalDetailsComponent } from './components/festival/festival-details/festival-details.component';
+import { MessageComponent } from './components/shared/message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FestivalsListComponent,
+    FestivalDetailsComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
